b5620bfa0f6b52450a383a51e1769fcdfc0e9b18
hj940079 <hj940709@gmail.com>
2016-10-11 23:39:59 +0300

    mouse done

---
+++ mouse.py
@@ -0,0 +1,30 @@
+import socket,time,linecache,random
+
+s = socket.socket()
+
+def init():
+#randomly select node
+#pick the port
+	port = 0
+	for line in linecache.getlines('port_number'):
+		str_arr = line.strip().split(' ')
+		if len(str_arr)>0 and str_arr[1] == 'mouse':
+			port = int(str_arr[0])
+	ukkos = linecache.getlines('ukkonodes')
+	host = ukkos[random.randrange(0,len(ukkos))]
+	return (host,port)
+
+(host,port) = init() 
+s.bind((host, port))       
+s.listen(5)
+while True:
+	conn, addr = s.accept()
+	msg = str(conn.recv(1024)).strip()
+	msg = string[2:len(string)-1]
+	if msg!='' :
+		continue
+	
+	if msg =='MEOW'
+		time.sleep(8)
+		conn.send(byte('OUCH','UTF-8')
+	print(s.recv(1024))
